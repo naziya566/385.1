16f81f4ae8e37d8ce9cba14d3401b480eec0eecf
NAAZ <naziyamsas@gmail.com>
2026-09-09 18:04:28 -0400

    Updated Current_Syntax

--- main.py
+++ main.py
@@ -1,42 +1,59 @@
-print('Hello World')
-# dataset
-# square brakets represent a list
-# list, indexed (ordered), list of values
-data = [1, 5, 6, 6, 8, 13, 14, 18]
-print('Given Data:', data)
+# Into to Python (Redux)
 
-length = len(data)
-print('Length:', length)
+# Single Line Comment - inline comment
 
-# 1. Calculate the mean sum(all values) / length
-def findAvg(list):
-    sum = 0
+'''
+MultiLine Comment / Document Comments
 
-    for val in list:
-        sum = sum + val
+Python is read top --> bottom, left to right - sequencing
 
-    return sum / length
+loosely typed - we do not declare datatypes
+'''
 
-avg = findAvg(data)
+# To Run Python: python3 <name of file>
 
-print('Avg:', avg)
+print('Hello, World!')
 
-# 2. Calculate Squared differences
-#   # Find the difference
-#   # Square the difference
-sqr_diff = []
+"""
+Variable - container with a label on it. You can put ANY information inside you want and you use the name to grab that value/info
+Box with a label
 
-for val in data:
-     output_val = (val - avg)**2
-     sqr_diff.append(output_val)
+Naming conventions for variables:
+- Cannot START with a number
+- Cannot include spaces
+- Cannot include most symbols (can use underscore)
+- Should be as clear as necessary
+- Cannot use reserved keywords
+- Case-sensitive
+- Must start with a letter or underscore
+- Insdustry Standard: use snake_case or camelCase.
+"""
 
-print('Squared Differences:', sqr_diff)
+# Primitive Data Types
 
-# 3. Average of squared differences (Variance)
-variance = findAvg(sqr_diff)
-print('Variance:', variance)
+# Number:
+# int - integers. whole numbers. Discrete values.
+# float - floating point decimal numbers.
+fav_number = 13
 
-# 4. Take the sqaure root of the variance = StDev
-standard_dev = variance**0.5
+# Strings - text, ALWAYS surrounded with quotes. Double or single quotes.
+name = 'Dylan'
+cat_name = "Thumbs"
+text = "Joey says, \"HelloWorld\", when he greets the day"
 
-print('Standard Deviation:', standard_dev)
+# Boolean - bools. True/False. 0,1.
+# Do not put quotes on a boolean, you will make a string, and all populated string are True
+speak_french = False
+speak_english = True
+
+# print(name, "favorite number", fav_number)
+# Formatted strings (f-string) allow the direct injection of variables/values into a string.
+# Use f'' to declare a string type, then curly braces {} to surround the dynamic values (variables)
+# print(f'{name} has a pet named {cat_name}')
+
+
+# input() function - accepts input from the user in the terminal. To call (invoke, run) a function you use parenthases
+name = input("What is your name? ")
+print(f'Hello {name}')
+age = input('How old are you? ')
+print(f'You are {age} years old!')
